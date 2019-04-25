90b5ed3f022075ece691883ffa89c4231977e64f
liyudong <1768756152@qq.com>
2019-04-25 19:40:22 +0800

    --

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ssl-demo
 ssl-demo
+
+
+keytool -genkey -keystore example.keystore -storepass example -keypass example -dname "CN=localhost, OU=test, O=test, L=test, S=test, C=test" -keyalg EC -sigalg SHA256withECDSA
+
+keytool -export -keystore example.keystore -file examplle-jks.cer -storepass example
+
+keytool -import -keystore example.truststore -file examplle-jks.cer -storepass examplle -keypass examplle -noprompt
+
---
+++ jks-demo/src/main/java/hardworking/hanxiong/SpringSecurityRsa.java
@@ -0,0 +1,30 @@
+package hardworking.hanxiong;
+
+/**
+ * @author liyd
+ **/
+public class SpringSecurityRsa {
+
+  public static void main(String[] args) {
+
+//    TextEncryptor encryptor = new RsaSecretEncryptor();
+//
+//
+//
+//    String cipher = encryptor.encrypt("话说贾琏听凤姐儿说有话商量，\n" +
+//            "因止步问是何话。凤姐道：“二十一是薛妹妹的生日，你到底怎么样呢？”\n" +
+//            "贾琏道：“我知道怎么样！你连多少大生日都料理过了，这会子倒没了主意？”\n" +
+//            "凤姐道：“大生日料理，不过是有一定的则例在那里。如今他这生日，\n" +
+//            "大又不是，小又不是，所以和你商量。”贾琏听了，低头想了半日道：“你今儿糊涂了。\n" +
+//            "现有比例，那林妹妹就是例。往年怎么给林妹妹过的，\n" +
+//            "如今也照依给薛妹妹过就是了。”\n" +
+//            "凤姐听了，冷笑道：“我难道连这个也不知道？\n" +
+//            "我原也这么想定了。但昨儿听见老太太说，问起大家的年纪生日来，听见薛\n");
+//    String message = encryptor.decrypt(cipher);
+//    System.out.println(cipher);
+//    System.out.println(message);
+
+
+  }
+
+}
